12475647e24a672f69e93b0faccbbc8afbf07811
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-20 21:17:35 +0900

    upper case counter test is completed

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -21,6 +21,11 @@ public class CustomCalculatorTest {
         assertTrue(result==25);
         assertThat(result, is(25));
         System.out.println("result :: " + result);
+
+        int result1 = customCalculator.add(12, 14);
+        assertFalse(result1==34);
+        assertThat(result1, is(26));
+        System.out.println("result :: " + result1);
     }
 
     //빼기 테스트 작성
@@ -33,6 +38,13 @@ public class CustomCalculatorTest {
         assertThat(result, is(13));
         //assert true, false는 공존할 수 없다.
         System.out.println("result :: " + result);
+
+        int result1 = customCalculator.subtract(45,13);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertFalse(result1==23);
+        assertThat(result1, is(32));
+        //assert true, false는 공존할 수 없다.
+        System.out.println("result :: " + result1);
     }
     @Test
     public void substractByFalse() {
@@ -40,6 +52,10 @@ public class CustomCalculatorTest {
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
         assertFalse(result==12);
+
+        int result1 = customCalculator.subtract(23,6);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertFalse(result1==12);
     }
 
     //곱하기 테스트 작성
@@ -50,6 +66,12 @@ public class CustomCalculatorTest {
         /* 이곳에 테스트 코드를 작성하세요. */
         assertTrue(result==45);
         System.out.println("result :: " + result);
+
+        int result1 = customCalculator.multiply(6,9);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result1==54);
+        assertThat(result1, is(54));
+        System.out.println("result :: " + result1);
     }
 
     //나누기 테스트 작성
@@ -58,16 +80,26 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result==5);
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
+
+        int result1 = customCalculator.divide(55,5);
+        /* 이곳에 테스트 코드를 작성하세요. */
+        assertFalse(result1==12);
+        assertThat(result1, is(11));
+        System.out.println("result :: " + result1);
     }
     @Test(timeout = 4000)
     public void timeInMethodTest() throws InterruptedException {
         Thread.sleep(5000);
     }
+    //시간이 초과하기 때문에 test를 통과할 수 없다.
     @Test(timeout = 3000)
     public void 시간체크() throws InterruptedException{
         Thread.sleep(2000);
     }
+    //시간을 초과하지 않기 때문에 test를 이상 없이 진행한다.
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIsEmptyIndexOutofBoundException(){
         new ArrayList<Object>().get(0);
@@ -84,6 +116,8 @@ public class CustomCalculatorTest {
     @Test
     public void isEmptyArray() {
         ArrayList<Object> myList = new ArrayList<>();
+        int result = myList.size();
         assertThat(myList, is(empty()));
+        assertTrue(result==0);
     }
 }
--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -17,16 +17,24 @@ public class UpperCaseCounterTest {
 
         int numberOfUpperCaseCharinString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         assertThat(numberOfUpperCaseCharinString, is(0));
+        assertEquals(numberOfUpperCaseCharinString,0);
+        assertTrue(numberOfUpperCaseCharinString==0);
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        System.out.println("result :: " + numberOfUpperCaseCharinString);
+        //null인 문자열에는 당연히 uppercase인 문자가 있을 수 없다. 따라서 모든 test를 이상없이 통과 한다.
     }
 
-    //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
+    //빈값울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
+        assertEquals(numberOfUpperCaseCharactersInString,0);
+        assertTrue(numberOfUpperCaseCharactersInString==0);
+
+        System.out.println("result :: " + numberOfUpperCaseCharactersInString);
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -41,7 +49,10 @@ public class UpperCaseCounterTest {
         //assertFalse로 틀리는 값을 넣어 테스트 작성
         assertThat(numberOfUpperCaseCharactersInString, is(10));
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+
+        //문자열에 대문자만 10개가 포함되어 있음으로 test를 이상 없이 통과 한다.
     }
+
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
     @Test
@@ -51,22 +62,35 @@ public class UpperCaseCounterTest {
 
         assertTrue(result == 6);
         //assertTrue로 맞는 테스트 코드 작성
+        assertFalse(result==1);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertThat(result, is(6));
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
         System.out.println("result :: " + result);
+
+        //대문자가 6개 포함된 문자열 임으로 test를 이상 없이 통과 한다.
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
+        new ArrayList<Object>().indexOf(0);
+        new ArrayList<Object>().remove(0);
     }
 
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+    @Test(timeout = 6000)
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
+
+        Thread.sleep(1000);
+        System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
+
+        Thread.sleep(500);
+        System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
+        //3thread의 시간을 다 더해도 6000미만이기 때문에 test를 이상 없이 통과 한다.
     }
 }
